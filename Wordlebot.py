74af40f0c51dea4e1185d8095346e9684d62c9df
Jeffrey Zhou <jeffreyzhou@berkeley.edu>
2022-07-19 02:33:19 -0700

    basic evaluate

--- Wordlebot.py
+++ Wordlebot.py
@@ -10,7 +10,7 @@ class Gamestate:
         self.allwords = words.readlines()
       
     def update(self, guess, outcome, allwords):
-        Gamestate.pastwords.append(guess)
+        Gamestate.pastwords.append(guess.upper())
         guessword = [char for char in guess]
         curroutcome = [char for char in outcome]
         self.updatecolour(guessword, curroutcome)
@@ -54,33 +54,44 @@ class Gamestate:
     
     def updatecolour(self, guessword, curroutcome):
         for i in range(5):
-            if curroutcome[i] == 'G':
+            if curroutcome[i] == 'G' and guessword[i] not in Gamestate.greenletters:
                 Gamestate.greenletters.append(guessword[i])
-            elif curroutcome[i] == 'Y':
+            elif curroutcome[i] == 'Y' and guessword[i] not in Gamestate.yellowletters:
                 Gamestate.yellowletters.append(guessword[i])
                 
     
-    def evaluate():
-        return
+    def evaluate(self, allwords):
+        
+        return allwords[0]
     
-    def printer(self):
+    def printer(self, bestguess):
         attempt_count = ['1st', '2nd', '3rd', '4th', '5th', '6th']
         print("\n" + "="*75)
         print("The possible words are as follows: \n")
         print(*self.allwords, sep= " ")
         print("\nThere are {} possible words after the {} guess.".format(len(self.allwords), attempt_count[len(Gamestate.pastwords) - 1]))
+        print("\nThe bot recommends playing ---> '{}'".format(bestguess.upper()))
         print("="*75 + "\n")
     
-    def status(self, attempt, ):
+    def printstats(self):
+        print("\n" + "="*75)
+        print("\nThanks for using WordleBot! I hope you've had fun!\n")
+        print("***** You played the following {} words: *****".format(len(Gamestate.pastwords)))
+        print(*Gamestate.pastwords, sep= ", ")
+        print("")
+        print("Starting with 5757 words, you've narrowed it down to {} words in {} guesses".format(len(self.allwords), len(Gamestate.pastwords)))
+        print("\n" + "="*75)
+        exit()
+    
+    def status(self, attempt):
         if (attempt == 0):
             print("\nHey! This is WordleBot! What's your first guess?")
         else:
             print("Please input {} guess!".format(attempt.upper()))
-            print("If puzzle has been solved, input 'DONE'")
+            print("If puzzle has been solved (5 greens), input 'DONE' for some stats!")
         guess = input("Guess: ")
         if guess.lower() == "done":
-            print("\nCongrats! Thanks for using WordleBot!")
-            exit()
+            self.printstats()
         if len(guess) != 5:
             print("ERROR: Words must be 5 letters long\n")
             exit()
@@ -96,25 +107,30 @@ class Gamestate:
 game = Gamestate()
 
 status = game.status(attempt=0)
-game.update(status[0].lower(), status[1].upper(), game.allwords)   
-game.printer()
+game.update(status[0].lower(), status[1].upper(), game.allwords)
+bestguess1 = game.evaluate(game.allwords)   
+game.printer(bestguess1)
 
 status2 = game.status(attempt= 'second')
 game.update(status2[0].lower(), status2[1].upper(), game.allwords)
-game.printer()   
+bestguess2 = game.evaluate(game.allwords)  
+game.printer(bestguess2)   
 
 status3 = game.status(attempt= 'third')
 game.update(status3[0].lower(), status3[1].upper(), game.allwords)
-game.printer()  
+bestguess3 = game.evaluate(game.allwords)  
+game.printer(bestguess3)  
 
 status4 = game.status(attempt= 'fourth')
 game.update(status4[0].lower(), status4[1].upper(), game.allwords)
-game.printer()
+bestguess4 = game.evaluate(game.allwords)  
+game.printer(bestguess4)
 
 status5 = game.status(attempt= 'fifth')
 game.update(status5[0].lower(), status5[1].upper(), game.allwords)
-game.printer()  
+bestguess5 = game.evaluate(game.allwords)  
+game.printer(bestguess5)  
 
 status6 = game.status(attempt= 'sixth')
 game.update(status6[0].lower(), status6[1].upper(), game.allwords)
-game.printer()  
+game.printstats
